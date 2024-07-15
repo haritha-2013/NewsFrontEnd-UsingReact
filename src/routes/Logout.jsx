70b28d2f5847f3ea47fff38387a6e16d39da00fb
Haritha <haritha.abhilash2013@gmail.com>
2024-07-16 01:18:30 +0530

    changes in Logout.jsx

--- src/routes/Logout.jsx
+++ src/routes/Logout.jsx
@@ -4,26 +4,22 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { changeLoginStatus } from "../features/login/loginSlice";
 
-function Logout (props) {
-
-    const navigate = useNavigate () 
-    const dispatch = useDispatch()
-    
-useEffect (() => {
-    axios.get(`${import.meta.env.VITE_API_BASE_URL}/auth/logout`, {
-        withCredentials:true
-    }
-    
-    )
-    .then (res => {
-        dispatch (changeLoginStatus(false))
-        navigate ('/login')
-
-    })
-    .catch(error => {
-        console.log(error)
-    })
-}), []
+function Logout() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+  
+    useEffect(() => {
+      axios.get(`${import.meta.env.VITE_API_BASE_URL}/auth/logout`, {
+        withCredentials: true
+      })
+      .then(res => {
+        dispatch(changeLoginStatus(false));
+        navigate('/login');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }, []);
 
     return (
         <div>
